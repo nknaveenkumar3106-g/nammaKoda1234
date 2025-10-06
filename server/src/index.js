f0c480bb5eb4e5ff401a56b99f8fd79531357068
nknaveenkumar3106-g <nknaveenkumar3106@gmail.com>
2025-10-06 15:18:53 +0530

    donee

--- server/src/index.js
+++ server/src/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/namma_kodai';
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 mongoose
   .connect(MONGO_URI)
